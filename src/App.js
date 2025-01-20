658e6de5c22be5ef0a74c07104c11ca5fbe61c89
Mashudu Luvhengo <mluvhengo25@gmail.com>
2025-01-20 15:35:46 +0200

    added github repo url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+      <a href="https//github.com/mashudu08/react-weather-app" target="_blank">Project open-sourced code on Github</a> by Mashudu Luvhengo
+      </footer>
+      
     </div>
   );
 }
