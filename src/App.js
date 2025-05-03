a3b423c7358b8e48b288d5dc67008d25d40c66d2
Mashudu Luvhengo <mluvhengo25@gmail.com>
2025-05-03 12:24:51 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Weather App
-      <footer>
-      <a href="https//github.com/mashudu08/react-weather-app" target="_blank">Project open-sourced code on Github</a> by Mashudu Luvhengo
-      </footer>
-      
-    </div>
+      <div className="App">
+          <div className="container">
+              <h1>Weather App</h1>
+              <footer>
+                  <a href="https//github.com/mashudu08/react-weather-app" target="_blank">Project open-sourced code on
+                      Github</a> by Mashudu Luvhengo
+              </footer>
+          </div>
+      </div>
   );
 }
 
